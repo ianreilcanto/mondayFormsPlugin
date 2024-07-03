4c08ce34bfc9a9cf7a4c654b88ef949346af680d
ianreilcanto <canto.ian.reil@gmail.com>
2024-07-03 12:27:19 +0800

    initial commit

---
+++ wordpress_data/wp-content/plugins/mondayForms/mondayForms.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package Akismet
+ */
+/*
+Plugin Name: monday.com Forms
+Plugin URI: 
+Description: Used to Integrate monday.com Forms to Wordpress
+Version: 1.0
+Requires at least: 5.8
+Requires PHP: 5.6.20
+Author: MintConsulting - Ian
+License: GPLv2 or later
+Text Domain: monday.com Forms
+*/
